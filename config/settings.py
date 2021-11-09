d1fe38116b804bfd949999b832845850e1c00596
Mathieu <dupycournau@gmail.com>
2021-11-09 10:07:21 +0100

    create custom user model

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client.apps.ClientConfig',
+    'contract.apps.ContractConfig',
+    'event.apps.EventConfig',
+    'userteam.apps.UserteamConfig'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'userteam.Employe'
--- userteam/models.py
+++ userteam/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Employe(AbstractUser):
+    team = models.ForeignKey(to=Team)
