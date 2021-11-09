726975a9cc652cf6c113af8f6b72047e14603eb4
Mathieu <dupycournau@gmail.com>
2021-11-09 10:00:30 +0100

    create event / eventstatus model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from client.models import Client
+
+
+class EventStatus(models.Model):
+    libelle = models.CharField(max_length=30)
+
+class Event(models.Model):
+    client = models.ForeignKey(to=Client)
+    supportContact = models.ForeignKey(to=Employe)
+    eventStatus = models.ForeignKey(to=EventStatus)
+    attendees = models.IntegerField()
+    eventDate = models.DateTimeField()
+    note = models.CharField(max_length=20)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now_add=True)
