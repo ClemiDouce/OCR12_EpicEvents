8242ee77df8e0bf802b087757154056f8ae03028
Mathieu <dupycournau@gmail.com>
2021-11-09 09:56:33 +0100

    create client model

--- client/models.py
+++ client/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    firstName = models.CharField(max_length=25)
+    lastName = models.CharField(max_length=25)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=20)
+    companyName = models.CharField(max_length=250)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now_add=True)
