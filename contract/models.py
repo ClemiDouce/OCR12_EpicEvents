d8e19487d489d8996748806d2364b3e171254616
Mathieu <dupycournau@gmail.com>
2021-11-09 09:58:34 +0100

    create contract model

--- contract/models.py
+++ contract/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from client.models import Client
+
+
+class Contract(models.Model):
+    saleContact = models.ForeignKey(to=Employe)
+    client = models.ForeignKey(to=Client)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField()
+    amount = models.FloatField()
+    paymentDue = models.DateTimeField()
