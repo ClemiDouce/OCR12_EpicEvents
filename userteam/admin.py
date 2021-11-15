a9854276f1a0a920e16be12c30e3af7eaa318524
Mathieu <dupycournau@gmail.com>
2021-11-15 15:43:36 +0100

    add superuser permission on save

--- userteam/admin.py
+++ userteam/admin.py
@@ -9,7 +9,16 @@ admin.site.register(Team)
 
 class UserForm(forms.ModelForm):
     class Meta:
-        fields = ["first_name", "last_name", "username", "email", "password", "team"]
+        fields = ["first_name", "last_name", "username", "email", "password", "team", "is_staff"]
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserForm, self).save(commit=False)
+        # user.set_password(self.cleaned_data["password"])
+        user.is_superuser = self.cleaned_data['is_staff']
+        if commit:
+            user.save()
+        return user
 
 @admin.register(Employe)
 class UserAdmin(admin.ModelAdmin):
