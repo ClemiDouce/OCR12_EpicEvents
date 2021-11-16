18b81b3839db524fc46150677c28c1d1677d0f21
Mathieu <dupycournau@gmail.com>
2021-11-16 15:17:19 +0100

    pep8 modification

--- config/urls.py
+++ config/urls.py
@@ -20,12 +20,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from contract.views import SelfContractViewset
 from event.views import SelfEventViewset
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('client.urls')),
-    path('api/events', SelfEventViewset.as_view({'get':'list'}), name="self-events"),
-    path('api/contracts', SelfContractViewset.as_view({'get':'list'}), name="self-contracts")
+    path('api/events', SelfEventViewset.as_view({'get': 'list'}), name="self-events"),
+    path('api/contracts', SelfContractViewset.as_view({'get': 'list'}), name="self-contracts")
 ]
