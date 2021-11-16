1e2bedf787862ca2c0e56457c8476d87485e5488
Mathieu <dupycournau@gmail.com>
2021-11-16 15:16:04 +0100

    pep8

--- event/serializers.py
+++ event/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
+
 from .models import Event, EventStatus
+
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('__all__')
+        fields = '__all__'
+
 
 class EventStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventStatus
-        fields = ('__all__')
+        fields = '__all__'
